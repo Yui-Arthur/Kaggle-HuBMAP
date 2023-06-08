46b5d4941a084f060a07ee2751f7c61344816b74
Yui-Arthur <arthur80633@gmail.com>
2023-06-08 21:19:22 +0800

    add training mask-rcnn code & modify get mask

--- utils/gen_mask.py
+++ utils/gen_mask.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pandas as pd    
 import cv2
+import torch
 import numpy as np 
 
 
@@ -9,30 +10,52 @@ import numpy as np
 def gen_mask(annotations):
     boxes = []
     masks = []
+    # area = []
     for i in range(len(annotations)):
 
         if annotations[i]['type'] == "blood_vessel":
             pts = np.array(annotations[i]['coordinates'])
+            # print(pts[0][0])
             min_xy = np.min(pts, axis=1)[0]
             max_xy = np.max(pts, axis=1)[0]
-            boxes += [min_xy.tolist() + max_xy.tolist()]
-            # + max_xy.tolist()
 
+            boxes += [np.concatenate((min_xy, max_xy), axis=0)]
 
-            # print(boxes)
-            # print(max_xy - min_xy)
             mask = np.zeros((512,512), dtype=np.uint8)
             cv2.fillPoly(mask, pts, 1)
 
-            masks += [mask]
+            # area.append((max_xy[0] - min_xy[0]) * (max_xy[1] - min_xy[1]))
 
+            masks += [mask]
+        # break
+    boxes = torch.as_tensor(boxes, dtype=torch.float32)
+    masks = torch.as_tensor(masks, dtype=torch.uint8)
+    area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+    return boxes , masks , area
+    print(area)
+    print(boxes)
     print(masks)
 
-
-
+def get_valid_data(labels):
+    valid_data = []
+    for i in range(len(labels)):
+        for j in range(len(labels.iloc[i]['annotations'])):
+            if labels.iloc[i]['annotations'][j]['type'] == "blood_vessel":
+                valid_data.append(labels.iloc[i]['id'])
+                break
+    
+    return valid_data
+    print(valid_data)
 if __name__ == '__main__':
     ROOT = Path("hubmap-hacking-the-human-vasculature")
     labels = pd.read_json(ROOT / "polygons.jsonl" , lines=True)
-    for i in range(len(labels)):
-        gen_mask(labels.iloc[i]['annotations'])
-        break
+    id_list = get_valid_data(labels)
+    print("359bb86fa14" in id_list)
+    print(labels[labels['id'] == "359bb86fa14"])
+    # print(id_list)
+    # for i in range(len(labels)):
+    #     # print(i)
+    #     if labels.iloc[i]['id'] in id_list:
+    #         gen_mask(labels.iloc[i]['annotations'])
+    #     # break
